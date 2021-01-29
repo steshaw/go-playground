1d2daa3e9b3ec80b8e944889e46b41f82babb8d2
Steven Shaw <steven@steshaw.org>
2021-01-29 16:54:29 +1000

    range

--- 2min/2min.go
+++ 2min/2min.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func PrintRange(is []int) {
+	fmt.Println("range {")
+	for x, i := range is {
+		fmt.Printf("  %d : %d\n", x, i)
+	}
+	fmt.Println("}")
+}
+
 func main() {
 	fmt.Println("Hi")
 
@@ -18,8 +26,33 @@ func main() {
 	}
 
 	{
-		for i := 0; i < 5; i++ {
-			fmt.Println("i = %s", i)
+		for i := 10; i < 15; i++ {
+			fmt.Printf("i = %d\n", i)
 		}
 	}
+
+	/*
+		{
+			for x, i := 10; i < 15; i++ {
+				fmt.Println(x, ": ", i)
+			}
+		}
+	*/
+
+	var as = [5]int{10, 11, 12, 13, 14}
+	fmt.Println(as)
+	{
+		for a := range as {
+			fmt.Printf("a = %d\n", a)
+		}
+	}
+	{
+		for i, a := range as {
+			fmt.Printf("%d: %d\n", i, a)
+		}
+	}
+	PrintRange([]int{})
+	PrintRange([]int{2})
+	PrintRange([]int{2, 4})
+	PrintRange([]int{2, 4, 6})
 }
