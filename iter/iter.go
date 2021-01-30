599b6eadebabdc7800c0e79d2925acfb72d80fe0
Steven Shaw <steven@steshaw.org>
2021-01-30 12:10:07 +1000

    Cannot implement early-exit with return

--- iter/iter.go
+++ iter/iter.go
@@ -12,12 +12,36 @@ func upTo(from, to int) func(func(int)) {
 	}
 }
 
-func main() {
-	fmt.Println("non-leaky iteration")
+func iter1() {
 	for {
-		upTo(2, 5)(func(a int) {
+		upTo(2, 5)(func(i int) {
+			fmt.Println(i)
 			// Cannot exit iteration early with "break".
 			// break
 		})
 	}
+}
+
+func iter2_aux() {
+	upTo(2, 5)(func(i int) {
+		fmt.Println(i)
+		// Try to break iteration with `return`.
+		// Unfortunately, here `return` only returns from the anonymous function
+		// and not from `iter2_aux`.
+		return
+	})
+}
+
+func iter2() {
+	for {
+		iter2_aux()
+	}
+}
+
+func main() {
+	fmt.Println("non-leaky iteration")
+	if false {
+		iter1()
+	}
+	iter2()
 }
