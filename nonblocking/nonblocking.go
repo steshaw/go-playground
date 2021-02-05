28cd967e1c1dc4800873bf8fc36a95587b02d2db
Steven Shaw <steven@steshaw.org>
2021-02-05 17:46:20 +1000

    Non-blocking send

--- nonblocking/nonblocking.go
+++ nonblocking/nonblocking.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func nonblockingReceive() {
+func nonBlockingRecv() {
+	fmt.Println("Non-blocking recv")
+	fmt.Println("-----------------")
 	ch := make(chan string, 1)
 	fmt.Println("ch", ch)
 
@@ -16,8 +21,51 @@ func nonblockingReceive() {
 			fmt.Println("no message on ch")
 		}
 	}
+	fmt.Println("---")
+}
+
+func nonBlockingSend() {
+	fmt.Println("Non-blocking send")
+	fmt.Println("-----------------")
+
+	ch := make(chan int)
+	go func() {
+		i := <-ch
+		fmt.Println("recv from ch", i)
+	}()
+
+	// Wait for go routine to start...
+	time.Sleep(50 * time.Millisecond)
+
+	// Hmm, a non-blocking send seems weird to do with a select block...
+	// But it's nice that send is symmetric to recv.
+
+	// This one can send.
+	select {
+	case ch <- 1:
+		fmt.Println("Able to send to ch")
+	default:
+		fmt.Println("Unable to send to ch")
+	}
+
+	// These cannot send as no-one is receiving.
+	select {
+	case ch <- 1:
+		fmt.Println("Able to send to ch")
+	default:
+		fmt.Println("Unable to send to ch")
+	}
+	select {
+	case ch <- 1:
+		fmt.Println("Able to send to ch")
+	default:
+		fmt.Println("Unable to send to ch")
+	}
+
+	fmt.Println("---")
 }
 
 func main() {
-	nonblockingReceive()
+	nonBlockingRecv()
+	nonBlockingSend()
 }
