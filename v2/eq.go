328074f37ed8c3de414f168982ad8850601349c0
Steven Shaw <steven@steshaw.org>
2021-10-16 14:51:40 +1000

    eq

---
+++ v2/eq.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Eq[T comparable](a T, b T) bool {
+  return a == b
+}
+
+func main() {
+  fmt.Println(Eq(1, 1))
+  fmt.Println(Eq(2, 1))
+  fmt.Println(Eq("foo", "foo"))
+  fmt.Println(Eq("foo", "fred"))
+  fmt.Println(Eq('a', 'a'))
+  fmt.Println(Eq('a', 'b'))
+}
