943f7c7b36343282c1c6b46241cab22a0b33cc50
Steven Shaw <steven@steshaw.org>
2021-11-04 11:53:53 +1000

    Recover panic + successful Container

--- v1/iface-struct/main.go
+++ v1/iface-struct/main.go
@@ -14,6 +14,22 @@ func doFoo(f Fooer) {
 	fmt.Println(f.Foo())
 }
 
-func main() {
+type foo struct{}
+
+func (foo) Foo() string {
+	return "foo!"
+}
+
+func panicky() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered r=%v\n", r)
+		}
+	}()
 	doFoo(Container{})
+}
+
+func main() {
+	panicky()
+	doFoo(Container{Fooer: foo{}})
 }
