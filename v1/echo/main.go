672c5fa4b8e7ac50214b73d06dcd502fc77e7119
Steven Shaw <steven@steshaw.org>
2021-11-23 08:26:15 +1000

    Use Join

--- v1/echo/main.go
+++ v1/echo/main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println(os.Args)
-	fmt.Println(len(os.Args))
-	var s, sep string
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
