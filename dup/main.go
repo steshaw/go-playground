e5e2e8eec7db175536be788896a47bfc821e7c84
Steven Shaw <steven@steshaw.org>
2021-11-26 15:25:20 +1000

    Extract countLines

--- dup/main.go
+++ dup/main.go
@@ -9,6 +9,15 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	countLines(os.Stdin, counts)
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		counts[scanner.Text()]++
@@ -16,10 +25,5 @@ func main() {
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal("Error scanning", err)
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
 	}
 }
