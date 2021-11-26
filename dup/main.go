312ced819e4886027d6d5565e429c07a0293dfe0
Steven Shaw <steven@steshaw.org>
2021-11-26 15:35:55 +1000

    Use system arguments

--- dup/main.go
+++ dup/main.go
@@ -9,7 +9,21 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	countLines(os.Stdin, counts)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		// Use stdin
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				log.Fatalf("dup: %v\n", err)
+			}
+			defer f.Close()
+			countLines(f, counts)
+		}
+	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
@@ -18,7 +32,7 @@ func main() {
 }
 
 func countLines(f *os.File, counts map[string]int) {
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		counts[scanner.Text()]++
 		// ignoring input.Err()
