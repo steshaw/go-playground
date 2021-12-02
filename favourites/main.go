2b2d8a6f3d333f084408a716a46dc39735007d62
Steven Shaw <steven@steshaw.org>
2021-12-02 17:26:03 +1000

    More favourites

--- favourites/main.go
+++ favourites/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func isFavourite1(s string) bool {
 	favs := map[string]struct{}{
@@ -39,12 +42,182 @@ func isFavourite2(s string) bool {
 	return result
 }
 
+func isFavouriteSwitch(s string) bool {
+	switch s {
+	case "A":
+		return true
+	case "B":
+		return true
+	case "C":
+		return true
+	case "D":
+		return true
+	case "E":
+		return true
+	case "M":
+		return true
+	case "N":
+		return true
+	case "X":
+		return true
+	case "Y":
+		return true
+	case "Z":
+		return true
+	default:
+		return false
+	}
+}
+
+func isFavouriteSwitchByte(s string) bool {
+	if len(s) != 1 {
+		return false
+	}
+	switch s[0] {
+	case 'A':
+		return true
+	case 'B':
+		return true
+	case 'C':
+		return true
+	case 'D':
+		return true
+	case 'E':
+		return true
+	case 'M':
+		return true
+	case 'N':
+		return true
+	case 'X':
+		return true
+	case 'Y':
+		return true
+	case 'Z':
+		return true
+	default:
+		return false
+	}
+}
+
+func isFavouriteContains(s string) bool {
+	favs := []string{
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"M",
+		"N",
+		"X",
+		"Y",
+		"Z",
+	}
+	for _, fav := range favs {
+		if s == fav {
+			return true
+		}
+	}
+	return false
+}
+
+var globalFavsSlice = []string{
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"M",
+	"N",
+	"X",
+	"Y",
+	"Z",
+}
+
+func isFavouriteContainsGlobal(s string) bool {
+	for _, fav := range globalFavsSlice {
+		if s == fav {
+			return true
+		}
+	}
+	return false
+}
+
+var globalFavsByte = []byte{
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'M',
+	'N',
+	'X',
+	'Y',
+	'Z',
+}
+
+func isFavouriteContainsByte(s string) bool {
+	if len(s) != 1 {
+		return false
+	}
+	b := s[0]
+	for _, fav := range globalFavsByte {
+		if b == fav {
+			return true
+		}
+	}
+	return false
+}
+
+var globalFavsRune = []rune{
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'M',
+	'N',
+	'X',
+	'Y',
+	'Z',
+}
+
+func isFavouriteContainsRune(s string) bool {
+	runes := []rune(s)
+	if len(runes) != 1 {
+		return false
+	}
+	r := runes[0]
+	for _, fav := range globalFavsRune {
+		if r == fav {
+			return true
+		}
+	}
+	return false
+}
+
+func isFavouriteContainsString(s string) bool {
+	return strings.Contains("ABCDEMNXYZ", s)
+}
+
 func main() {
 	for i := 0; i < 127; i++ {
 		s := "" + string(rune(i))
-		fmt.Printf("isFavourite(%q) = %v\n",
-			s,
+		results := []bool{
 			isFavourite1(s),
-		)
+			isFavourite2(s),
+			isFavouriteSwitch(s),
+			isFavouriteSwitchByte(s),
+			isFavouriteContains(s),
+			isFavouriteContainsGlobal(s),
+			isFavouriteContainsByte(s),
+			isFavouriteContainsString(s),
+			isFavouriteContainsRune(s),
+		}
+		fmt.Printf("isFavourite(%q) = %v\n", s, results)
+		for _, r := range results[1:] {
+			if results[0] != r {
+				panic("functions are not equivalent")
+			}
+		}
 	}
 }
--- favourites/main_test.go
+++ favourites/main_test.go
@@ -1,33 +1,53 @@
 package main
 
 import (
-	"math/rand"
 	"testing"
 )
 
 var result bool
 
-func randMinMax(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
-func randS() string {
-	//return string(rune(randMinMax(32, 126)))
-	return "A"
-}
-
-func BenchmarkIsFavourite1(b *testing.B) {
-	var r bool
+func benchmarkFav(
+	b *testing.B,
+	isFav func(string) bool,
+	s string,
+) {
 	for n := 0; n < b.N; n++ {
-		r = isFavourite1(randS())
+		isFav(s)
 	}
-	result = r
 }
 
-func BenchmarkIsFavourite2(b *testing.B) {
-	var r bool
-	for n := 0; n < b.N; n++ {
-		r = isFavourite2(randS())
-	}
-	result = r
-}
+func BenchmarkFavA1(b *testing.B)              { benchmarkFav(b, isFavourite1, "A") }
+func BenchmarkFavA2(b *testing.B)              { benchmarkFav(b, isFavourite2, "A") }
+func BenchmarkFavASwitch(b *testing.B)         { benchmarkFav(b, isFavouriteSwitch, "A") }
+func BenchmarkFavASwitchByte(b *testing.B)     { benchmarkFav(b, isFavouriteSwitchByte, "A") }
+func BenchmarkFavAContains(b *testing.B)       { benchmarkFav(b, isFavouriteContains, "A") }
+func BenchmarkFavAContainsGlobal(b *testing.B) { benchmarkFav(b, isFavouriteContainsGlobal, "A") }
+func BenchmarkFavAContainsString(b *testing.B) { benchmarkFav(b, isFavouriteContainsString, "A") }
+func BenchmarkFavAContainsRune(b *testing.B)   { benchmarkFav(b, isFavouriteContainsRune, "A") }
+
+func BenchmarkFavE1(b *testing.B)              { benchmarkFav(b, isFavourite1, "E") }
+func BenchmarkFavE2(b *testing.B)              { benchmarkFav(b, isFavourite2, "E") }
+func BenchmarkFavESwitch(b *testing.B)         { benchmarkFav(b, isFavouriteSwitch, "E") }
+func BenchmarkFavESwitchByte(b *testing.B)     { benchmarkFav(b, isFavouriteSwitchByte, "E") }
+func BenchmarkFavEContains(b *testing.B)       { benchmarkFav(b, isFavouriteContains, "E") }
+func BenchmarkFavEContainsGlobal(b *testing.B) { benchmarkFav(b, isFavouriteContainsGlobal, "E") }
+func BenchmarkFavEContainsString(b *testing.B) { benchmarkFav(b, isFavouriteContainsString, "E") }
+func BenchmarkFavEContainsRune(b *testing.B)   { benchmarkFav(b, isFavouriteContainsRune, "E") }
+
+func BenchmarkFavY1(b *testing.B)              { benchmarkFav(b, isFavourite1, "Y") }
+func BenchmarkFavY2(b *testing.B)              { benchmarkFav(b, isFavourite2, "Y") }
+func BenchmarkFavYSwitch(b *testing.B)         { benchmarkFav(b, isFavouriteSwitch, "Y") }
+func BenchmarkFavYSwitchByte(b *testing.B)     { benchmarkFav(b, isFavouriteSwitchByte, "Y") }
+func BenchmarkFavYContains(b *testing.B)       { benchmarkFav(b, isFavouriteContains, "Y") }
+func BenchmarkFavYContainsGlobal(b *testing.B) { benchmarkFav(b, isFavouriteContainsGlobal, "Y") }
+func BenchmarkFavYContainsString(b *testing.B) { benchmarkFav(b, isFavouriteContainsString, "Y") }
+func BenchmarkFavYContainsRune(b *testing.B)   { benchmarkFav(b, isFavouriteContainsRune, "Y") }
+
+func BenchmarkFava1(b *testing.B)              { benchmarkFav(b, isFavourite1, "a") }
+func BenchmarkFava2(b *testing.B)              { benchmarkFav(b, isFavourite2, "a") }
+func BenchmarkFavaSwitch(b *testing.B)         { benchmarkFav(b, isFavouriteSwitch, "a") }
+func BenchmarkFavaSwitchByte(b *testing.B)     { benchmarkFav(b, isFavouriteSwitchByte, "a") }
+func BenchmarkFavaContains(b *testing.B)       { benchmarkFav(b, isFavouriteContains, "a") }
+func BenchmarkFavaContainsGlobal(b *testing.B) { benchmarkFav(b, isFavouriteContainsGlobal, "a") }
+func BenchmarkFavaContainsString(b *testing.B) { benchmarkFav(b, isFavouriteContainsString, "a") }
+func BenchmarkFavaContainsRune(b *testing.B)   { benchmarkFav(b, isFavouriteContainsRune, "a") }
