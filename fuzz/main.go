cffa298283164745e89a467c736c6aca10712eb8
Steven Shaw <steven@steshaw.org>
2022-01-22 16:45:48 +1000

    Fuzz

---
+++ fuzz/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func reverse(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
+}
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev := reverse(input)
+	doubleRev := reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q\n", rev)
+	fmt.Printf("reversed again: %q\n", doubleRev)
+}
